94f922b868fe2d46148a6eb9c7f49e14625f375e
Hayun218 <gkahsdl@icloud.com>
2024-04-01 13:11:07 +0900

    [BOJ10814] stable_sort

--- cpp/Solved_Class 2/BOJ10814.cpp
+++ cpp/Solved_Class 2/BOJ10814.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <string>
 using namespace std;
 
+bool compare(pair<int, string> a, pair<int,string> b) {
+    return a.first < b.first;
+}
+
 int main() {
-    string map[100000][2];
-
-    int N, i;
-    scanf("%d", &N);
-
-    for(i = 0; i < N; i++) scanf("%s %s", &map[i][0], &map[i][1]);
+    // cin, cout 의 속도 높이기
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     
-    sort(&map[0][0], &map[N][0]);
+    int N;
+    cin >> N;
+    
+    // struct 구조체 형태로 만들어도 됨
+    pair<int, string> tmp;
+    vector<pair<int, string> > members;
 
-    for(i = 0; i < N; i++) 
-        printf("%s %s", map[i][0], map[i][1]);
+    for(int i = 0; i < N; i++) {
+        cin >> tmp.first >> tmp.second;
+        members.push_back(tmp);
+    }
 
+    // 기존 순서 유지하면서 sort 하는 함수
+    stable_sort(members.begin(), members.end(), compare);
 
+    for(int i = 0; i < N; i++) {
+        cout << members[i].first << ' ' << members[i].second << '\n';
+    }
     return 0;
 }
