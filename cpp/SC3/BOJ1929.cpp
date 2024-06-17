15f995d2af7eac4d37220241883cbb1ff63f7422
Hayun218 <gkahsdl@icloud.com>
2024-06-17 11:27:32 +0900

    reorganize

---
+++ cpp/SC3/BOJ1929.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+bool isPrime[1000000] = {true, };
+
+
+void checkPrime(int a) {
+    for(int i = 2; i <= a; i++) {
+        if(isPrime[i] == true) {
+           // for(int j = )
+        }
+
+    }
+}
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+
+
+
+
+    return 0;
+
+}
